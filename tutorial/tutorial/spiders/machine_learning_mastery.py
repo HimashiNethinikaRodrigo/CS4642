5f0e2c28357dbf8b431bec0aea039e959e3c9e20
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2018-06-18 21:25:32 +0530

    machine learning mastery blog scrape

--- tutorial/tutorial/spiders/machine_learning_mastery.py
+++ tutorial/tutorial/spiders/machine_learning_mastery.py
@@ -2,19 +2,21 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "mastery"
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://machinelearningmastery.com/blog/',
+            'https://machinelearningmastery.com/blog/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = '%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+
